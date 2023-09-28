6d65d7b64f0d1826663fae8e7d4f29a321b4cc42
Hamsa <hamsa.g@outlook.com>
2023-09-28 19:16:28 +0200

    Commit version with API

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 function App() {
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
   const [guests, setGuests] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-  const addGuest = (event) => {
-    if (event) event.preventDefault();
+  const addGuest = async (event) => {
+    if (event) {
+      event.preventDefault();
+    }
     if (firstName && lastName) {
-      const newGuest = { name: `${firstName} ${lastName}`, attending: false };
-      setGuests([...guests, newGuest]);
+      const newGuest = {
+        firstName: firstName,
+        lastName: lastName,
+        attending: false,
+      };
+
+      // Send the new guest data to the API
+      try {
+        const response = await fetch('http://localhost:4000/guests', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(newGuest),
+        });
+
+        const createdGuest = await response.json();
+        setGuests((prevGuests) => [...prevGuests, createdGuest]);
+      } catch (error) {
+        console.error('Error adding guest:', error);
+      }
+
       setFirstName('');
       setLastName('');
     }
@@ -22,53 +45,127 @@ function App() {
     }
   };
 
-  const toggleAttendance = (index) => {
-    const updateGuests = [...guests];
-    updateGuests[index].attending = !updateGuests[index].attending;
-    setGuests(updateGuests);
+  const toggleAttendance = async (index) => {
+    const guestToUpdate = guests[index];
+
+    try {
+      const response = await fetch(
+        `http://localhost:4000/guests/${guestToUpdate.id}`,
+        {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ attending: !guestToUpdate.attending }),
+        },
+      );
+
+      console.log(response.statusText);
+
+      if (response.ok) {
+        console.log(response.status, response.statusText);
+        const updatedGuest = await response.json();
+        const updatedGuests = [...guests];
+        updatedGuests[index] = updatedGuest;
+        setGuests(updatedGuests);
+      } else {
+        console.error(
+          'Error updating guest attendance status:',
+          response.status,
+          response.statusText,
+          await response.text(),
+        );
+      }
+    } catch (error) {
+      console.error('Error updating guest attendance status:', error);
+    }
   };
 
-  const deleteGuest = (indexToDelete) => {
-    const updatedGuests = guests.filter((_, index) => index !== indexToDelete);
-    setGuests(updatedGuests);
+  const deleteGuest = async (indexToDelete) => {
+    const guestToDelete = guests[indexToDelete];
+
+    try {
+      const response = await fetch(
+        `http://localhost:4000/guests/${guestToDelete.id}`,
+        {
+          method: 'DELETE',
+        },
+      );
+
+      if (response.ok) {
+        const updatedGuests = guests.filter(
+          (_, index) => index !== indexToDelete,
+        );
+        setGuests(updatedGuests);
+      } else {
+        console.error('Error deleting guest:', await response.text());
+      }
+    } catch (error) {
+      console.error('Error deleting guest:', error);
+    }
   };
+
+  useEffect(() => {
+    const fetchGuests = async () => {
+      try {
+        const response = await fetch('http://localhost:4000/guests');
+        const data = await response.json();
+        setGuests(data);
+      } catch (error) {
+        console.error('Error fetching guests:', error);
+      } finally {
+        setIsLoading(false); // <-- This line sets loading to false after fetch
+      }
+    };
+
+    fetchGuests();
+  }, []);
 
   return (
     <div>
-      <form onSubmit={addGuest}>
-        <label>
-          First Name:
-          <input
-            value={firstName}
-            onChange={(e) => setFirstName(e.target.value)}
-            onKeyDown={handleKeydown}
-          />
-        </label>
-        <label>
-          Last Name:
-          <input
-            value={lastName}
-            onChange={(e) => setLastName(e.target.value)}
-            onKeyDown={handleKeydown}
-          />
-        </label>
-      </form>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          <form onSubmit={addGuest}>
+            <label>
+              First Name:
+              <input
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+                onKeyDown={handleKeydown}
+                disabled={isLoading}
+              />
+            </label>
+            <label>
+              Last Name:
+              <input
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+                onKeyDown={handleKeydown}
+                disabled={isLoading}
+              />
+            </label>
+          </form>
 
-      <ul>
-        {guests.map((guest, index) => (
-          <li key={index}>
-            {guest.name}
-            <input
-              type="checkbox"
-              checked={guest.attending}
-              onChange={() => toggleAttendance(index)}
-              aria-label={`${guest.name} attending status`}
-            />
-            {guest.attending ? ' attending' : ' not attending'}
-            <button onClick={() => deleteGuest(index)}>Remove</button>
-          </li>
-        ))}
-      </ul>
+          <ul>
+            {guests.map((guest, index) => (
+              // eslint-disable-next-line react/no-array-index-key
+              <li key={index}>
+                {`${guest.firstName} ${guest.lastName}`}
+                <input
+                  type="checkbox"
+                  checked={guest.attending}
+                  onChange={() => toggleAttendance(index)}
+                  aria-label={`$${guest.firstName} ${guest.lastName} attending status`}
+                />
+                {guest.attending ? 'attending' : 'not attending'}
+                <button onClick={() => deleteGuest(index)}>Remove</button>
+              </li>
+            ))}
+          </ul>
+        </>
+      )}
     </div>
   );
 }
